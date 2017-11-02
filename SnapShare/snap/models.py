6bdb327603555e2251c9f79e835715b409b14ee1
root <aadil.abbas96@gmail.com>
2017-11-02 03:08:17 -0400

    Added Models

--- SnapShare/snap/models.py
+++ SnapShare/snap/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Models):
+    username = models.CharField(max_length = 20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+class Image(models.Models):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    image = models.ImageField()
+    likes = models.IntegerField()
